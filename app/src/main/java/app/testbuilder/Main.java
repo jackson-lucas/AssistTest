ecc210b862585f2899748d0672014afd655ca8c8
Jackson Lucas <jackson7br@gmail.com>
2015-01-18 03:11:04 -0400

    Assist Pergunta 1 agora verifica resposta do usuário

--- app/src/main/java/app/testbuilder/Main.java
+++ app/src/main/java/app/testbuilder/Main.java
@@ -3,8 +3,11 @@ package app.testbuilder;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 import java.util.Date;
 import java.sql.Timestamp;
 
@@ -20,6 +23,18 @@ public class Main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // START Retrieve data from another activity
+        Intent intent = getIntent();
+
+        if(intent != null) {
+            // Não funcionando, o porquê eu não sei
+            Boolean isClean = intent.getBooleanExtra("INTEGRO", false);
+            Log.d("isClean", isClean+"");
+            if(isClean) {
+                Toast.makeText(getApplicationContext(), "Parabéns por não utilizar drogas!", Toast.LENGTH_LONG);
+            }
+        }
 
         btnIniciar = (Button) findViewById(R.id.btnTeste);
 
