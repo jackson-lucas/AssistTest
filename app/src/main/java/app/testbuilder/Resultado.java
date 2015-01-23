0c44fd639a6107698ec5b98515b6a5fb2a6d963f
Jackson Lucas <jackson7br@gmail.com>
2015-01-23 03:43:16 -0400

    Tentativa de mostrar resultado
    Apesar de não está aparecendo de forma correta irei fazer esse commit
    pois, tem funções que podem ser utilizadas.

--- app/src/main/java/app/testbuilder/Resultado.java
+++ app/src/main/java/app/testbuilder/Resultado.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import org.json.JSONObject;
@@ -17,11 +20,11 @@ import java.util.List;
 
 import app.testbuilder.br.com.TestBuilder.Model.Assist;
 
-// TODO Update no SQLite ao finalizar
 public class Resultado extends ActionBarActivity {
 
     Assist assist;
-    int[] resultado;
+    int[] resultados;
+    String[] substancias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +37,7 @@ public class Resultado extends ActionBarActivity {
         if(intent != null) {
             assist = intent.getParcelableExtra("ASSIST");
 
-            resultado = assist.getResultado();
-
-            for(int result : resultado) {
-
-                Log.i("resultado: ", result+"");
-            }
+            resultados = assist.getResultado();
         }
 
         Button confirmButton = (Button) findViewById(R.id.button);
@@ -52,6 +50,71 @@ public class Resultado extends ActionBarActivity {
                 finish();
             }
         });
+
+        substancias = getResources().getStringArray(R.array.substancias);
+        Log.i("substancias", substancias.length+"");
+
+        showResult();
+
+    }
+
+    public void showResult() {
+        TableLayout tabelaResulado = (TableLayout) findViewById(R.id.table_result);
+        String p1;
+        int resultado;
+        String intervencao;
+
+        TableRow.LayoutParams textLayoutParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+        //textLayoutParams.gravity = Gravity.CENTER_HORIZONTAL;
+
+        p1 = assist.getP1();
+        for(int index = 0, indexResultado = 0; index < p1.length(); index++) {
+            if(p1.charAt(index) == '1') {
+                TableRow tableRow = new TableRow(this);
+                tableRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+                tableRow.setGravity(Gravity.CENTER_HORIZONTAL);
+
+                TextView substancia = new TextView(this);
+                substancia.setText(substancias[index]);
+                substancia.setLayoutParams(textLayoutParams);
+
+                resultado = resultados[indexResultado];
+                Log.i("Resultado: ", resultado+"");
+                indexResultado++;
+
+                TextView pontuacao = new TextView(this);
+                pontuacao.setText(resultado+"");
+                pontuacao.setLayoutParams(textLayoutParams);
+
+                if(substancias[index].equals("bebidas alcoólicas")) {
+                    if(resultado < 11) {
+                        intervencao = "Nenhuma intervenção";
+                    } else if(resultado < 27) {
+                        intervencao = "Intervenção breve";
+                    } else {
+                        intervencao = "Tratamento intensivo";
+                    }
+                } else {
+                    if(resultado < 4) {
+                        intervencao = "Nenhuma intervenção";
+                    } else if(resultado < 27) {
+                        intervencao = "Intervenção breve";
+                    } else {
+                        intervencao = "Tratamento intensivo";
+                    }
+                }
+
+                TextView recomendacao = new TextView(this);
+                recomendacao.setText(intervencao);
+                recomendacao.setLayoutParams(textLayoutParams);
+
+                tableRow.addView(substancia);
+                tableRow.addView(pontuacao);
+                tableRow.addView(recomendacao);
+
+                tabelaResulado.addView(tableRow);
+            };
+        }
 
     }
 }
