628a45f881ca1798cf5403bd2ecb7e52ae5b6354
Jackson Lucas <jackson7br@gmail.com>
2015-01-26 13:48:05 -0400

    ApiHelper deletado + Main modificado p/ teste próprio

--- app/src/main/java/app/testbuilder/Main.java
+++ app/src/main/java/app/testbuilder/Main.java
@@ -1,12 +1,30 @@
 package app.testbuilder;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Main extends ActionBarActivity {
 
@@ -53,9 +71,19 @@ public class Main extends ActionBarActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_send_data) {
-            Intent i = new Intent(Main.this, Register.class);
+            /*Intent i = new Intent(Main.this, Register.class);
             startActivity(i);
             finish();
+            */
+            // call AsynTask to perform network operation on separate thread
+            if(isConnected()) {
+
+                new HttpAsyncTask().execute("http://www.testbuilder.com.br/testbuilder/post_usuario.php");
+                //new HttpAsyncTask().execute("http://hmkcode.appspot.com/jsonservlet");
+            } else {
+
+                Toast.makeText(getBaseContext(), "Servidor não encontrado!", Toast.LENGTH_LONG).show();
+            }
         }
 
         return super.onOptionsItemSelected(item);
@@ -65,4 +93,111 @@ public class Main extends ActionBarActivity {
     public void onBackPressed() {
         moveTaskToBack(true);
     }
+
+    // TODO(Ideal) server deve retornar uma mensagem de erro quando: JSON incompativel. E msg de sucesso quando compatível
+    // TODO(a ser feito) server deve retornar uma mensagem de recebido. Erros em JSON devem ser verificados por nós
+    // iso-8859-1 o servidor está lendo assim? e não UTF-8?
+    // TODO fazer JSON para enviar todos os dados do banco de dados
+    // Methods for HTTPRequest
+    public static String POST(String url, Person person){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // 1. create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // 2. make POST request to the given URL
+            HttpPost httpPost = new HttpPost(url);
+
+            String json = "";
+
+            // 3. build jsonObject
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.accumulate("name", person.name);
+            jsonObject.accumulate("country", person.country);
+            jsonObject.accumulate("twitter", person.twitter);
+
+            // 4. convert JSONObject to JSON to String
+            json = jsonObject.toString();
+
+            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+            // ObjectMapper mapper = new ObjectMapper();
+            // json = mapper.writeValueAsString(person);
+
+            // 5. set json to StringEntity
+            StringEntity se = new StringEntity(json);
+
+            // 6. set httpPost Entity
+            httpPost.setEntity(se);
+
+            // 7. Set some headers to inform server about the type of the content
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+
+            // 8. Execute POST request to the given URL
+            HttpResponse httpResponse = httpclient.execute(httpPost);
+
+            // 9. receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // 10. convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Não funcionou";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        // 11. return result
+        return result;
+    }
+
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+
+    // Just for test
+    public class Person {
+        String  name, country, twitter;
+    }
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            Person person = new Person();
+            person.name = ("Jackson");
+            person.country = ("Brazil");
+            person.twitter = ("Not Found");
+
+            return POST(urls[0],person);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Dados Enviados!", Toast.LENGTH_LONG).show();
+            Log.i("HTTP REQUEST RESULTADO", result);
+        }
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+
 }
--- app/src/main/java/app/testbuilder/br/com/TestBuilder/Utilities/ApiHelper.java
+++
@@ -1,8 +0,0 @@
-package app.testbuilder.br.com.TestBuilder.Utilities;
-
-/**
- * Created by jacksonlima on 1/22/15.
- */
-public class ApiHelper {
-    // Deve ser utilizada para acessar o banco de dados e transformá-los em JSON para enviar ao servidor
-}
